03a280a13562ba5ecd03841d76c3065562053668
Jerrick Backous <77847817+JerrickBackous@users.noreply.github.com>
2022-11-22 12:11:15 -0600

    dev commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,11 +20,11 @@
 ##
 golem::fill_desc(
   pkg_name = "college.betting.analyzer", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
+  pkg_title = "College Betting Analyzer", # The Title of the package containing the App
+  pkg_description = "Tools to help with college betting.", # The Description of the package containing the App
+  author_first_name = "Jerrick", # Your First Name
+  author_last_name = "Backous", # Your Last Name
+  author_email = "jerrick.backous20@gmail.com", # Your Email
   repo_url = NULL # The URL of the GitHub Repo (optional)
 )
 
@@ -33,11 +33,11 @@ golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license("Golem User") # You can set another license here
+usethis::use_mit_license("Jerrick Backous") # You can set another license here
 usethis::use_readme_rmd(open = FALSE)
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
-usethis::use_code_of_conduct(contact = "Golem User")
+usethis::use_code_of_conduct(contact = "Jerrick Backous")
 usethis::use_lifecycle_badge("Experimental")
 usethis::use_news_md(open = FALSE)
 
@@ -50,7 +50,7 @@ golem::use_recommended_tests()
 
 ## Favicon ----
 # If you want to change the favicon (default is golem's one)
-golem::use_favicon() # path = "path/to/ico". Can be an online file.
+golem::use_favicon("https://campus2canton.com/wp-content/uploads/2022/01/cropped-CC-GM-resized-125.png") # path = "path/to/ico". Can be an online file.
 # golem::remove_favicon() # Uncomment to remove the default favicon
 
 ## Add helper functions ----
