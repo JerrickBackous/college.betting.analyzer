53d798ee91de04237576dc0aa09f6f806b614d7b
Jerrick Backous <77847817+JerrickBackous@users.noreply.github.com>
2022-12-07 15:04:44 -0600

    add gitattributes

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -33,7 +33,7 @@ golem::add_shinyserver_file()
 
 ## Docker ----
 ## If you want to deploy via a generic Dockerfile
-golem::add_dockerfile_with_renv()
+golem::add_dockerfile()
 
 ## If you want to deploy to ShinyProxy
 golem::add_dockerfile_with_renv_shinyproxy()
